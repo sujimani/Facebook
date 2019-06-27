8c6c21a8fb2d6f059a6952269d117ce7cd15ae13
sujimani <50094713+sujimani@users.noreply.github.com>
2019-06-27 18:01:09 +0530

    Update Data.java

--- Facebook/src/main/java/filtemap/Data.java
+++ Facebook/src/main/java/filtemap/Data.java
@@ -7,7 +7,7 @@ public static HashMap<String,String> gertuserinfo()
 {
 	HashMap<String ,String> usermap=new HashMap<String,String>();
 	usermap.put("customer", "8526481962_Kasi1947");
-	usermap.put("mankakkakdad", "monika_1947");
+	usermap.put("manksdsdsdsdsdsdakkakdad", "monika_1947");
 	return usermap;
 }
 }
